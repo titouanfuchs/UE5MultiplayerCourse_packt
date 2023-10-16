1d533b7ba02f31a8784afe9769c73438a959a8e6
titouanfuchs <developper@titouanfuchs.fr>
2023-10-16 13:57:33 +0200

    chore: created project and imported all deps

---
+++ Source/MultiplayerCourse/MultiplayerCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiplayerCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiplayerCourse, "MultiplayerCourse" );
