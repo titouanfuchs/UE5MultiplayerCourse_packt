bccc4e7f4e57d61bdf3bdaac72dec3a9392e6830
titouanfuchs <developper@titouanfuchs.fr>
2023-10-18 13:56:37 +0200

    chore: created US_GameState class

---
+++ Source/MultiplayerCourse/GameState/US_GameState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "US_GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERCOURSE_API AUS_GameState : public AGameState
+{
+	GENERATED_BODY()
+};
