f26a647cbac2cfda66c6953d07d947c86bfb2772
titouanfuchs <developper@titouanfuchs.fr>
2023-10-18 13:47:05 +0200

    chore: created US_GameInstance class

---
+++ Source/MultiplayerCourse/GameInstance/US_GameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "US_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERCOURSE_API UUS_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
