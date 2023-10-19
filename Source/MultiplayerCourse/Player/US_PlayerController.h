86999401861d3e21b415d0f1f840ab40ce8a3ac2
titouanfuchs <developper@titouanfuchs.fr>
2023-10-19 13:42:35 +0200

    chore: created player related classes

---
+++ Source/MultiplayerCourse/Player/US_PlayerController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "US_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERCOURSE_API AUS_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
---
+++ Source/MultiplayerCourse/Player/US_PlayerState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "US_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERCOURSE_API AUS_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+};
