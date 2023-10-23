0148c25f1deabf11cc5071820d40b549432f6b92
titouanfuchs <developper@titouanfuchs.fr>
2023-10-23 13:27:26 +0200

    chore: defined all default classes for the GameMode

--- Source/MultiplayerCourse/GameMode/US_Gamemode.cpp
+++ Source/MultiplayerCourse/GameMode/US_Gamemode.cpp
@@ -2,10 +2,19 @@
 
 
 #include "US_Gamemode.h"
+#include "UObject/ConstructorHelpers.h"
 
 #include "MultiplayerCourse/GameState/US_GameState.h"
+#include "MultiplayerCourse/Player/US_PlayerController.h"
+#include "MultiplayerCourse/Player/US_PlayerState.h"
 
 AUS_Gamemode::AUS_Gamemode()
 {
 	GameStateClass = AUS_GameState::StaticClass();
+	PlayerStateClass = AUS_PlayerState::StaticClass();
+	PlayerControllerClass = AUS_PlayerController::StaticClass();
+
+	static ConstructorHelpers::FClassFinder<APawn>	PlayerPawnBPClass(TEXT("/Game/Blueprints/Player/BP_Character"));
+
+	if (PlayerPawnBPClass.Class != nullptr) DefaultPawnClass = PlayerPawnBPClass.Class;
 }
